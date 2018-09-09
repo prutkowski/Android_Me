ef67bc223ade67cc8fcec8e4022c10cd060346e5
Paweł Rutkowski <pawel.rutkowski@btskyrise.com>
2018-09-09 20:18:59 +0200

    Using ImageSwitcher for animating image changes

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,31 +1,36 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewSwitcher;
 
 import com.example.android.android_me.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BodyPartFragment extends Fragment {
@@ -35,8 +40,9 @@ public class BodyPartFragment extends Fragment {
     // Tag for logging
     private static final String TAG = "BodyPartFragment";
 
-    // Variables to store a list of image resources and the index of the image that this fragment displays
-    private List<Integer> mImageIds;
+    ImageSwitcher imageView = null;
+
+    private List<Integer> mImageIds = new ArrayList<>();
     private int mListIndex;
 
     /**
@@ -51,39 +57,53 @@ public class BodyPartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        // Inflate the Android-Me fragment layout
         View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        imageView = (ImageSwitcher) rootView.findViewById(R.id.body_part_image_view);
+        imageView.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView imageView = new ImageView(getActivity().getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+                return imageView;
+            }
+        });
+        Animation in  = AnimationUtils.loadAnimation(getActivity(), R.anim.left_to_right_in);
+        Animation out = AnimationUtils.loadAnimation(getActivity(), R.anim.left_to_right_out);
+        imageView.setInAnimation(in);
+        imageView.setOutAnimation(out);
 
-        // Get a reference to the ImageView in the fragment layout
-        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        showImage();
 
-        // If a list of image ids exists, set the image resource to the correct item in that list
-        // Otherwise, create a Log statement that indicates that the list was not found
-        if(mImageIds != null){
-            // Set the image resource to the list item at the stored index
-            imageView.setImageResource(mImageIds.get(mListIndex));
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increaseImageIndex();
+                showImage();
+            }
+        });
 
-            // TODO (1) Set a click listener on the image view and on a click increment the list index and set the image resource
-            // TODO (2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
-
-        } else {
-            Log.v(TAG, "This fragment has a null list of image id's");
-        }
-
-        // Return the rootView
         return rootView;
     }
 
-    // Setter methods for keeping track of the list images this fragment can display and which image
-    // in the list is currently being displayed
+    private void increaseImageIndex() {
+        if (mListIndex < mImageIds.size() - 1) {
+            mListIndex++;
+        } else
+            mListIndex = 0;
+    }
+
+    private void showImage() {
+        if (mListIndex > -1 && mImageIds.size() > mListIndex) {
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        }
+    }
 
     public void setImageIds(List<Integer> imageIds) {
-        mImageIds = imageIds;
+        if (imageIds != null)
+            mImageIds = imageIds;
     }
 
     public void setListIndex(int index) {
         mListIndex = index;
     }
-
-    // TODO (4) Override onSaveInstanceState and save the current state of this fragment
 }
